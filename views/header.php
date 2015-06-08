e3941071891fa13e91a0c177c8c5bbb129a093be
sanyisasha <molnar.sandor.benjamin@gmail.com>
2015-06-08 06:03:03 +0200

    Login modal
    06.08. 6:02

--- views/header.php
+++ views/header.php
@@ -40,16 +40,59 @@
     <!-- Right Nav Section -->
     <ul class="right">
       <li><a href="#">asd</a></li>
-      <li><a href="#" data-dropdown="login" data-options="align:bottom">Belépés</a></li>
+      <li><a href="#" data-reveal-id="login">Belépés</a></li>
     </ul>
-    <div id="login" class="f-dropdown small content" data-dropdown-content>
-      
-    </div>
+    <div id="login" class="reveal-modal small" data-reveal aria-labelledby="firstModalTitle" aria-hidden="true" role="dialog">
+    <h2>Belépés</h2>
+    <form method="POST" action="<?php echo URL."login" ?>">
+    <div class="row">
+            <label>Felhasználónév</label>
+            <input type="text" name="username" placeholder="email@pelda.hu" tabindex="1"/>
+        </div>
+        <div class="row">
+            <label>Jelszó</label>
+            <input type="password" name="password" placeholder="********" tabindex="2"/>
+        </div>
+        <div class="row">
+            <input type="submit" class="button small success" value="Belépés" tabindex="3"/>
+            <a href="#" data-reveal-id="register" class="button small alert">Regisztráció</a>
+        </div>
+    </form>
+  <a class="close-reveal-modal" aria-label="Close"><i class="fa fa-times"></i></a>
+</div>
+
+<div id="register" class="reveal-modal small" data-reveal aria-labelledby="firstModalTitle" aria-hidden="true" role="dialog">
+    <h2>Regisztráció</h2>
+    <form method="POST" action="<?php echo URL."login" ?>">
+        <div class="row">
+            <label>Felhasználónév</label>
+            <input type="text" name="username" placeholder="Felhasznalonev" tabindex="1"/>
+        </div>
+         <div class="row">
+            <label>Email</label>
+            <input type="email" name="email" placeholder="email@pelda.hu" tabindex="2"/>
+        </div>
+        <div class="row">
+            <label>Jelszó</label>
+            <input type="password" name="password" placeholder="********" tabindex="3"/>
+        </div>
+        <div class="row">
+            <label>Jelszó újra</label>
+            <input type="password" name="password2" placeholder="********" tabindex="4"/>
+        </div>
+        <div class="row">
+            <input type="submit" class="button small success" value="Regisztráció" tabindex="4"/>
+            <a href="#" data-reveal-id="login" class="button small alert">Belépés</a>
+        </div>
+    </form>
+  <a class="close-reveal-modal" aria-label="Close"><i class="fa fa-times"></i></a>
+</div>
     <!-- Left Nav Section -->
     <ul class="left">
       <li><a href="<?php echo URL ?>">Főoldal</a></li>
     </ul>
   </section>
+
 </nav>
 </div>
 
